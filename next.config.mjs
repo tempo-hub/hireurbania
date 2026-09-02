f7d3d1eab3eca10c863d950539076d191b08b6c7
Shaiphali23 <shaiphalijaiswal978@gmail.com>
2026-09-02 12:40:45 +0530

    fixed vercel build error

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,30 @@
 const nextConfig = {
   trailingSlash: false,
   reactStrictMode: true,
+  swcMinify: true,
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+  images: {
+    unoptimized: false,
+    domains: ['firebasestorage.googleapis.com', 'localhost'],
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
